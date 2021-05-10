a7966fb7e3ddc11321e1431c27e67b3007c3709a
MSarazua <sarazuamaria@gmail.com>
2021-05-10 22:45:59 +0000

    Mostrar y modificar productos SAP

--- TestApi/Controllers/SapController.cs
+++ TestApi/Controllers/SapController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -8,6 +10,7 @@ using System.Web.Http;
 using System.Web.Http.Results;
 using System.Web.Mvc;
 using TestApi.Utils;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
 using RouteAttribute = System.Web.Http.RouteAttribute;
 
@@ -15,8 +18,10 @@ namespace TestApi.Controllers
 {
     public class SapController : ApiController
     {
+        #region Variables
         //Instancia de la clase que devolverá la respuesta
         CallResponse responseCall = new CallResponse();
+        #endregion
         //Función pública tipo respuesta HTTP
         [HttpPost]
         //Defino la ruta
@@ -45,17 +50,70 @@ namespace TestApi.Controllers
                 responseCall.Description = company.GetLastErrorDescription().ToString();
 
             }
-
             return Ok(responseCall);
-
         }
 
-       
+        //Editar datos
+        //Función pública tipo respuesta HTTP
+        [HttpPost]
+        //Defino la ruta
+        [Route("api/UpdateItem")]
+        public IHttpActionResult UpdateItem(ItemDetails itemDetails)
+        {
+            //Inicializo mi conexión a SAP
+            SAPConnection conncetion = new SAPConnection();
+            SAPbobsCOM.Company company = conncetion.OpenConnection();
 
-        //public IHttpActionResult Ok(CallResponse callResponse)
-        //{
-        //  return Ok(callResponse);
-        //}
+            SAPbobsCOM.Items oItems;
+            oItems = company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.oItems);
+
+            if (oItems.GetByKey(itemDetails.ItemCode))
+            {
+                oItems.ItemCode = itemDetails.ItemCode;
+                oItems.ItemName = itemDetails.ItemName;
+                int status = oItems.Update();
+
+                //Compruebo si el guardado se ha realizado correctamente
+                if (status == 0)
+                {
+                    responseCall.RespCode = "00";
+                    responseCall.Description = "Guardado correctamente";
+                }
+                else
+                {
+                    responseCall.RespCode = "99";
+                    responseCall.Description = company.GetLastErrorDescription().ToString();
+
+                }
+            }
+            else
+            {
+                responseCall.RespCode = "90";
+                responseCall.Description = "El artículo no se encuentra en SAP";
+
+            }
+
+            return Ok(responseCall);
+        }
+
+        //Mostrar datos
+        [HttpGet]
+        [Route("api/getItems")]
+        public HttpResponseMessage getItems()
+        {
+            DataSet ds = new DataSet();
+            DataTable itemsData;
+            OdbcCommand cmd;
+
+            using (OdbcConnection conn = new OdbcConnection(@"Driver={SQL Server};Server=PRUEBASTUSAP;Database=SBO_IDEACODEX;uid=sa;pwd=Soporte@2021"))
+            {
+                string query = "Select ItemName, ItemCode from OITM";
+                cmd = new OdbcCommand(query, conn);
+                OdbcDataAdapter da = new OdbcDataAdapter(cmd);
+                da.Fill(ds, "Items");
+            }
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, ds);
+        }
 
         public class ItemDetails
         {
